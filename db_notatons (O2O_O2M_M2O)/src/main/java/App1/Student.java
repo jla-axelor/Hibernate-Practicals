be0040a2c6373819f4cd4655bea1b0a2e3c18a6e
jla-axelor <jaydeep.axelor@gmail.com>
2021-08-20 11:56:28 +0530

    relation notation commit

---
+++ db_notatons (O2O_O2M_M2O)/src/main/java/App1/Student.java
@@ -0,0 +1,76 @@
+package App1;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Transient;
+
+//create database with this name , also we can create db with another name with name method 
+@Entity
+public class Student {
+	//consider it as primary key of the table
+	@Id
+	private int sid;
+	//we can ignore column with transient 
+	
+	private  Names names;
+	private int marks;
+	
+	
+	//OneToOne Relation mapping  
+	//Student class and Books class
+	@OneToOne
+	private Books books;
+	
+	//OneToMany with Laptop class
+	@OneToMany(mappedBy = "student")
+ 	private List<Laptop> laptop;
+	
+	
+	
+	
+	
+	public List<Laptop> getLaptop() {
+		return laptop;
+	}
+	public void setLaptop(List<Laptop> laptop) {
+		this.laptop = laptop;
+	}
+	public Books getBooks() {
+		return books;
+	}
+	public void setBooks(Books books) {
+		this.books = books;
+	}
+	public int getSid() {
+		return sid;
+	}
+	public void setSid(int sid) {
+		this.sid = sid;
+	}
+	public Names getNames() {
+		return names;
+	}
+	public void setNames(Names names) {
+		this.names = names;
+	}
+	public int getMarks() {
+		return marks;
+	}
+	public void setMarks(int marks) {
+		this.marks = marks;
+	}
+	@Override
+	public String toString() {
+		return "Student [sid=" + sid + ", names=" + names + ", marks=" + marks + ", books=" + books + ", laptop="
+				+ laptop + "]";
+	}
+
+	
+	
+	
+	
+}
